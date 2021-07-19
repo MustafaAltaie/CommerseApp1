d122873d32c6bcbb18f3e6c1541b66015e0411dd
Mustafa <mustafaphoto111@gmail.com>
2021-07-19 14:04:47 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyP = require('body-parser');
+const path = require('path');
+const controllers = require('./controllers/controllers');
+
+var app = express();
+var port = process.env.port || 3000;
+app.listen(port);
+
+app.use(bodyP.urlencoded({
+    extended: true
+}));
+
+app.engine('html', exphbs({
+    extname: "html",
+    defaultLayout: "mainLayout"
+}));
+
+app.set('view engine', "html")
+
+app.use('', controllers);
+
+app.use(express.static(path.join(__dirname, '')));
